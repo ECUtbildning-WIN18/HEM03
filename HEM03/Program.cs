c93ad9c8acc674d16f1c95a84fcd234858cee395
Robert Tublen <robert.tublen@junction7.se>
2018-09-16 19:19:52 +0200

    Add project files.

---
+++ HEM03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HEM03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
